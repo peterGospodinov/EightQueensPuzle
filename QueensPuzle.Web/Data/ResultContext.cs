7ffc302b0138093b7c52b8bc184e7ed065f88ba3
peterGospodinov <peter.gospodinov@gmail.com>
2024-11-14 12:21:58 +0200

    commit

---
+++ QueensPuzle.Web/Data/ResultContext.cs
@@ -0,0 +1,12 @@
+﻿using EightQueensPuzleConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace QueensPuzle.Web.Data
+{
+    public class ResultContext : DbContext
+    {
+        public ResultContext(DbContextOptions<ResultContext> options) : base(options) { }
+
+        public DbSet<SolutionResult> SolutionResults { get; set; }
+    }
+}
